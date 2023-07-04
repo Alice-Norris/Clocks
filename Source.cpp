2bd918ecf6b3a3aed1f5438682ef2276c92fad99
alice <alice.norris@snhu.edu>
2023-07-04 19:21:37 -0400

    Cleaned up code, added comments

--- Source.cpp
+++ Source.cpp
@@ -1,142 +1,127 @@
+// Copyright 2023 Alice Norris. Subject to MIT License.
+
 #include <stdio.h>
 #include <iostream>
 #include <string>
 #include <ctime>
 
-using namespace std;
+// Handles writing the clock to the terminal.
+void PrintClock(std::string reg_time, std::string mil_time);
 
-//Begin function declarations
+// Clears the terminal screen, provided by instructor in course.
+void ClearScreen();
 
-//handles writing the clock to the terminal. 
-void PrintClock(string TwelveHourTime, string TwentyFourHourTime);
+// Prints user menu, handles user input.
+void Menu(int choice, int* hour_mod, int* min_mod, int* sec_mod);
 
-//clears the terminal screen, provided in 03/13/2021, 2:47 PM announcement
-void ClearScreen(); 
+// Gets current system time.
+void UpdateTime(tm* time_ptr);
 
-//This prints the user menu, takes user input, and returns it as an integer.
-void Menu(int &UserChoice, int &HourMod, int &MinuteMod, int &SecondMod);
+// Modifies current time of passed tm pointer using modifier values.
+void ModifyTime(tm* time_ptr, int hour_mod, int min_mod, int sec_mod);
 
-//Returns formatted time as a string, takes boolean TwelveHourFormat as an argument.
-//If TwelveHourFormat is true, time returned uses the 12-hour clock.
-//If false, time returned uses the 24-hour clock
-void UpdateTime(tm* Time);
+// Converts a tm struct into a time string using strftime, 12/24 hour clock
+// specified by mil_time bool (true for 24, false for 12).
+void GetTimeString(bool mil_time, std::string* time_str, tm* time_ptr);
 
-void ModifyTime(tm* TimePointer, int HourMod, int MinuteMod, int SecondMod);
-
-string GetTimeString(int HoursOnClock, tm* TimePointer);
-
-//End function declarations
-//begin program
 int main() {
-    int UserChoice = 0;
-    int HourMod = 0;
-    int MinuteMod = 0;
-    int SecondMod = 0;
-    string TwelveHourTime;
-    string TwentyFourHourTime;
-    tm Time;
-    tm* TimePointer = &Time;
+    int choice = 0; // user choice
+    // Modifier, used to apply user set modifications to the time.
+    int hour_mod = 0; 
+    int min_mod = 0;
+    int sec_mod = 0;
+    // Both strings are used at once in PrintClock to print 12h and 24h clocks.
+    std::string reg_time;
+    std::string mil_time;
+    // tm struct, to hold the actual time data.
+    tm time;
     do {
-        //PrintClock(TwelveHourTime, TwentyFourHourTime);
-
-        UpdateTime(TimePointer);
-        ModifyTime(TimePointer, HourMod, MinuteMod, SecondMod);
-        TwelveHourTime = GetTimeString(12, TimePointer);
-        TwentyFourHourTime = GetTimeString(24, TimePointer);
-        PrintClock(TwelveHourTime, TwentyFourHourTime);
-        Menu(UserChoice, HourMod, MinuteMod, SecondMod);      
-    } 
-   while (UserChoice != 4);
+        UpdateTime(&time);
+        ModifyTime(&time, hour_mod, min_mod, sec_mod);
+        GetTimeString(false, &reg_time, &time);
+        GetTimeString(true, &mil_time, &time);
+        PrintClock(reg_time, mil_time);
+        Menu(choice, &hour_mod, &min_mod, &sec_mod);
+    } while (choice != 4);
 }
 
-void Menu(int &UserChoice, int &HourMod, int &MinuteMod, int &SecondMod) {
-    string MenuTopAndBottoom = "*************************";
-    cout << "\n" << MenuTopAndBottoom << endl;
-    cout << "* 1 - Add One Hour\t*" << endl;
-    cout << "* 2 - Add One Minute\t*" << endl;
-    cout << "* 3 - Add One Second\t*" << endl;
-    cout << "* 4 - Exit Program\t*" << endl;
-    cout << MenuTopAndBottoom << endl;
-    cin >> UserChoice;
+void Menu(int choice, int* hour_mod, int* min_mod, int* sec_mod) {
+    std::string menu_border = "*************************";
+    std::cout << "\n" << menu_border << std::endl
+              << "* 1 - Add One Hour\t*" << std::endl
+              << "* 2 - Add One Minute\t*" << std::endl
+              << "* 3 - Add One Second\t*" << std::endl
+              << "* 4 - Exit Program\t*" << std::endl
+              << menu_border << std::endl;
 
-    switch (UserChoice) {
+    std::cin >> choice;
+
+    switch (choice) {
     case 1:
-        if (HourMod < 23) {
-            HourMod += 1;
+        if (*hour_mod < 23) {
+            *hour_mod += 1;
+        } else {
+            hour_mod = 0;
         }
-        else {
-            HourMod = 0;
-        }
-        ClearScreen();
         break;
-
     case 2:
-        if (MinuteMod < 60) {
-            MinuteMod += 1;
+        if (*min_mod < 60) {
+            *min_mod += 1;
+        } else {
+            *min_mod = 0;
         }
-        else {
-            MinuteMod = 0;
-        }
-        ClearScreen();
         break;
-
     case 3:
-        if (SecondMod < 60) {
-            SecondMod += 1;
+        if (*sec_mod < 60) {
+            *sec_mod += 1;
+        } else {
+            *sec_mod = 0;
         }
-        else {
-            SecondMod = 0;
-        }
-        ClearScreen();
         break;
-
+    case 4: 
+      exit(0);
+    
     default:
-        ClearScreen();
-        cout << "Invalid Choice! Please choose from the menu." << endl;
+        std::cout << "Invalid Choice! Please choose from menu." << std::endl;
         break;
     }
+
+    ClearScreen();
 }
 
 
-void PrintClock(string TwelveHourTime, string TwentyFourHourTime) {
-    string ClockTopAndBottom = "**************************\t**************************";
-
-    cout << ClockTopAndBottom << endl;
-    cout << "*     12-Hour Clock      *\t*     24-Hour Clock      *" << endl;
-    cout << "*" << TwelveHourTime << "*\t*" << TwentyFourHourTime << "*" << endl;
-    cout << ClockTopAndBottom << endl;
+void PrintClock(std::string reg_time, std::string mil_time) {
+    std::string border = "*************************\t*************************";
+    std::cout << border << std::endl
+        << "*     12-Hour Clock     *\t*     24-Hour Clock     *" << std::endl
+        << "*      " << reg_time << "*\t*      " << mil_time
+        << "*" << std::endl<< border << std::endl;
 }
 
 void ClearScreen() {
     for (int i = 0; i < 40; ++i) {
-        cout << endl;
+        std::cout << std::endl;
     }
 }
 
-void UpdateTime(tm* Time) {
-    time_t currentTime = time(NULL);
-    localtime_s(Time, &currentTime);
+void UpdateTime(tm* time_ptr) {
+    time_t curr_time = time(NULL);
+    localtime_s(time_ptr, &curr_time);
 }
 
-void ModifyTime(tm* TimePointer, int HourMod, int MinuteMod, int SecondMod) {
-    TimePointer->tm_hour = ((TimePointer->tm_hour + HourMod) % 24);
-    TimePointer->tm_min = ((TimePointer->tm_min + MinuteMod) % 60);
-    TimePointer->tm_sec = ((TimePointer->tm_sec + SecondMod) % 60);
+void ModifyTime(tm* time_ptr, int hour_mod, int min_mod, int sec_mod) {
+    time_ptr->tm_hour = ((time_ptr->tm_hour + hour_mod) % 24);
+    time_ptr->tm_min = ((time_ptr->tm_min + min_mod) % 60);
+    time_ptr->tm_sec = ((time_ptr->tm_sec + sec_mod) % 60);
 }
 
-string GetTimeString(int HoursOnClock, tm* TimePointer){
-    char TimeChars[26];
-    if (HoursOnClock == 12) { //if user indicates a 12-hour-clock is needed
-        strftime(TimeChars, sizeof TimeChars, "      %I:%M:%S %p       ", TimePointer);
-        
+void GetTimeString(bool mil_time, std::string* time_str, tm* time_ptr) {
+    // Time should be no longer than 26 characters using these format strings.
+    char time_chars[26];
+    if (!mil_time) {
+        strftime(time_chars, sizeof time_chars, "\t%I:%M:%S %p\t", time_ptr);
+    } else {
+        strftime(time_chars, sizeof time_chars, "\t%H:%M:%S\t", time_ptr);
     }
-    else if (HoursOnClock == 24) {
-
-        strftime(TimeChars, sizeof TimeChars, "        %H:%M:%S        ", TimePointer);
-    }
-    else {
-        cout << "Something went wrong!";
-    }
-    string TimeString = TimeChars;
-    return TimeString;
+    *time_str = std::string(time_chars);
 }
